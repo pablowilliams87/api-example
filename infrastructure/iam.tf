84d8f82c00cf6c7efe04e185374badc325c5128f
unknown <pablo.williams87@gmail.com>
2020-11-01 22:53:05 -0300

    fix iam role

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -49,7 +49,4 @@ resource "aws_iam_instance_profile" "ecs-instance-profile" {
   name = "ecs-instance-profile"
   path = "/"
   role = aws_iam_role.ecs-instance-role.id
-  /*    provisioner "local-exec" {
-      command = "sleep 10"
-    }*/
 }
