8819130b40a12dc7307342cc53b63c6d030c477a
unknown <pablo.williams87@gmail.com>
2020-11-02 22:49:58 -0300

    .

--- tools/loadCSVToDatabase.sh
+++ tools/loadCSVToDatabase.sh
@@ -2,4 +2,3 @@
 
 docker exec -ti postgres psql -d environment_airq_measurand -U postgres -c "\copy environment_airq_measurand(timestamp,id_entity,so2,no2,co,o3,pm10,pm2_5) from '/tmp/environment_airq_measurand.csv' with csv"
 
-
