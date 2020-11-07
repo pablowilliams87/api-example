fb9ec192d523fbe26518b10721dce4c7db43a4c1
Pablo Williams <pablo.williams87@gmail.com>
2020-11-06 22:02:05 -0300

    .

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ WORKDIR /
 
 COPY app/ .
 
-RUN apt-get update && apt-get install -y libpq-dev gcc
-RUN pip install -r requirements.txt
+RUN apt-get update && \ 
+  apt-get install -y libpq-dev gcc && \
+  rm -rf /var/lib/apt/lists/* && \
+  pip install --no-cache-dir -r requirements.txt
 
 ENV DB_URI=postgresql://postgres:mysecretpassword@db-host:5432/environment_airq_measurand \
   URL_MEASUREMENTS=https://gist.githubusercontent.com/jvillarf/040c91397d779d4da02fff54708ca935/raw/f1dbbcbfbc4e3daace7d907a3cc5b716ef808014/environment_airq_measurand.csv \
--- app/app.py
+++ app/app.py
@@ -1,12 +1,13 @@
+# Import libraries
 import os
 import requests
 import pandas
-# import csv
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists, create_database, drop_database
 
+# Open DB connection
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:mysecretpassword@localhost:5432/environment_airq_measurand"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
@@ -14,14 +15,20 @@ db = SQLAlchemy(app)
 from models import EnvironmentAirqMeasurand
 
 
+# Define flask initdb command
 @app.cli.command("initdb")
 def initdb_command():
+  # Create database if not exist
   if not database_exists(os.getenv('DB_URI')):
     create_database(os.getenv('DB_URI'))
-#  db.create_all()
+
+  # Set url with URL_MEASUREMENTS envvar value
   url = os.getenv('URL_MEASUREMENTS')
+  # Dowbload csv file
   r = requests.get(url)
   open('/tmp/environment_airq_measurand.csv', 'wb').write(r.content)
+
+  # Read CSV and Load to Database
   file_name = '/tmp/environment_airq_measurand.csv'
   df = pandas.read_csv(file_name)
   engine=db.create_engine(os.getenv('DB_URI'),{})
@@ -30,6 +37,7 @@ def initdb_command():
 
 @app.route('/air_quality')
 def get_measurements():
+  # Query all air quality measurements
   measurements = EnvironmentAirqMeasurand.query.all()
   results = [{
     "TimeInstant": measurement.TimeInstant,
@@ -42,7 +50,8 @@ def get_measurements():
     "pm2_5": measurement.pm2_5
   } for measurement in measurements]
 
-  return {"measurements": results}
+  # Return air quality measurements
+  return {"air_quality_measurements": results}
 
 
 if __name__ == '__main__':
